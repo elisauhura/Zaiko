08740db0dd1d999272419b7ef7096ebd57dae476
Elisa Silva <elisa@silva.moe>
2021-10-16 15:26:53 -0300

    Initial Commit

---
+++ Zaiko/ZaikoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaikoApp.swift
+//  Zaiko
+//
+//  Created by Elisa Silva on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ZaikoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
